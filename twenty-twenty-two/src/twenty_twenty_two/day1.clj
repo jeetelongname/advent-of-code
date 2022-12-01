4d329bad692b2613ec5571e77ed31f889bb57aa9
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2022-12-01 23:37:16 +0000

    2022: clean up solution a little

--- twenty-twenty-two/src/twenty_twenty_two/day1.clj
+++ twenty-twenty-two/src/twenty_twenty_two/day1.clj
@@ -1,13 +1,16 @@
 (ns day1
-  (:require [clojure.string :as str]))
+  (:require
+   [clojure.string  :as string]))
 
 (defn parse-file
   "takes in the string in the puzzle form
   returns a list of list of numbers"
   [string]
-  (map (fn [lst] (map #(read-string %) lst))
-       (map  #(str/split % #"\n")
-             (str/split string #"\n\n"))))
+  (->> (string/split string #"\n\n")
+       (map #(string/split % #"\n"))
+       (map (partial map read-string))
+       (map (partial apply +))))
+
 (comment
   ;; part 1
   (reduce #(max %1 %2)
@@ -16,7 +19,6 @@
 
   (->> (slurp "input/day1.txt")
        parse-file
-       (map (partial apply +))
        (reduce #(max %1 %2)))
 
   ;; part 2
@@ -28,7 +30,6 @@
 
   (->> (slurp "input/day1.txt")
        parse-file
-       (map (partial apply +))
        sort
        reverse
        (take 3)
